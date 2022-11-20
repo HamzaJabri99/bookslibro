60e19baf3328bb334224be7c506df4a47561ee5e
unknown <jabri.oldd@gmail.com>
2022-11-20 17:12:28 +0100

    addBook functionality with thunkAPI

--- client/src/components/AddForm.js
+++ client/src/components/AddForm.js
@@ -1,35 +1,96 @@
-import React from 'react';
-
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addBook } from "../store/bookSlice";
 const Addform = () => {
-    return (
-        <div className='row'>
-            <div className='col-6 offset-3 mt-3'>
-                <h2>Insert Book</h2>
-                <form>
-                    <div className='form-group'>
-                        <label htmlFor='title'>Title</label>
-                        <input type='text' className='form-control' id='title' required />
-                    </div>
-                    <div className='form-group'>
-                        <label htmlFor='price'>Price</label>
-                        <input type='number' className='form-control' id='price' required />
-                    </div>
-                    <div className='form-group'>
-                        <label htmlFor='Description'>Description</label>
-                        <textarea
-                            className='form-control'
-                            id='Description'
-                            rows='3'
-                            required
-                        ></textarea>
-                    </div>
-                    <button type='submit' className='btn btn-primary'>
-                        Submit
-                    </button>
-                </form>
-            </div>
-        </div>
-    );
+  const [formErrors, setFormErrors] = useState([]);
+  const dispatch = useDispatch();
+  const [inputs, setInputs] = useState({
+    title: "",
+    price: "",
+    description: "",
+  });
+  const handleInputs = (e) => {
+    setInputs((prev) => ({ ...prev, [e.target.id]: e.target.value }));
+    console.log(inputs);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (inputs.title === "") {
+      setFormErrors((prev) => [...prev, "title is required"]);
+    }
+    if (inputs.price === "") {
+      setFormErrors((prev) => [...prev, "price is required"]);
+    }
+    if (inputs.description === "") {
+      setFormErrors((prev) => [...prev, "description is required"]);
+    } else {
+      dispatch(addBook(inputs));
+      setInputs({
+        title: "",
+        price: "",
+        description: "",
+      });
+      setFormErrors([]);
+    }
+  };
+  return (
+    <div className="row">
+      <div className="col-6 offset-3 mt-3">
+        <h2>Insert Book</h2>
+        <form>
+          <div className="form-group">
+            <label htmlFor="title">Title</label>
+
+            <input
+              type="text"
+              className="form-control"
+              id="title"
+              onChange={handleInputs}
+              value={inputs.title}
+            />
+            {formErrors[0] ? (
+              <span style={{ color: "red" }}>{formErrors[0]}</span>
+            ) : (
+              ""
+            )}
+          </div>
+          <div className="form-group">
+            <label htmlFor="price">Price</label>
+            <input
+              type="number"
+              className="form-control"
+              id="price"
+              onChange={handleInputs}
+              value={inputs.price}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="Description">Description</label>
+            <textarea
+              className="form-control"
+              id="description"
+              rows="3"
+              onChange={handleInputs}
+              value={inputs.description}
+            ></textarea>
+            {formErrors[0] ? (
+              <span style={{ color: "red" }}>{formErrors[2]}</span>
+            ) : (
+              ""
+            )}
+          </div>
+          <button
+            type="submit"
+            className="btn btn-primary"
+            onClick={handleSubmit}
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Addform;
--- client/src/store/bookSlice.js
+++ client/src/store/bookSlice.js
@@ -13,10 +13,27 @@ export const getBooks = createAsyncThunk('book/getBooks', async (args, thunkAPI)
         //dispatch({type:book/getBooks/rejected},payload:err)
     }
 });
+export const addBook = createAsyncThunk('book/addBook', async (bookData, thunkAPI) => {
+    const { rejectWithValue } = thunkAPI
+    try {
+        const resp = await fetch('http://localhost:3009/books', {
+            method: 'POST',
+            body: JSON.stringify(bookData),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8'
+            },
+        });
+        const data = await resp.json();
+        return data;
+    } catch (err) {
+        return rejectWithValue(err.message);
+    }
+})
 const bookSlice = createSlice({
     name: 'book',
     initialState: { books: [], isLoading: false, error: false },
     extraReducers: {
+        //getBooks 
         [getBooks.pending]: (state, action) => {
             state.isLoading = true;
             state.error = false;
@@ -26,6 +43,19 @@ const bookSlice = createSlice({
             state.books = action.payload
         },
         [getBooks.rejected]: (state, action) => {
+            state.isLoading = false;
+            state.error = true;
+        },
+        //addBook
+        [addBook.pending]: (state, action) => {
+            state.isLoading = true;
+            state.error = false;
+        },
+        [addBook.fulfilled]: (state, action) => {
+            state.isLoading = false;
+            state.books.push(action.payload)
+        },
+        [addBook.rejected]: (state, action) => {
             state.isLoading = false;
             state.error = true;
         }
